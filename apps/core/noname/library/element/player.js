4649eb673be12a96ea8bb73652af67d37305be2d
xizifu <2238983941@qq.com>
2026-04-29 22:45:02 +0800

    调整

--- apps/core/noname/library/element/player.js
+++ apps/core/noname/library/element/player.js
@@ -5233,8 +5233,8 @@ export class Player extends HTMLDivElement {
 					break;
 			}
 		}
-		// 要手牌或者特殊牌时需要区分 glows
-		const needGlowsCheck = hasH || hasS;
+		// 只有同时要手牌和特殊牌时才需要区分 glows
+		const needGlowsCheck = !hasH || !hasS;
 
 		// 一些简单的去重
 		let handDone = false;
